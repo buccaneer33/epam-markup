4f1c86d6e3a06a0474976a0fce2e01276b2f8963
buccaneer33 <buccaneer33@inbox.ru>
2018-06-13 16:34:21 +0400

    variables and themes finished

--- code/server.js
+++ code/server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var static = require('node-static');
-var file = new static.Server('.');
+var file = new static.Server('./dist', { cache: 0 });
 
 http.createServer(function(req, res) {
-  file.server(req, res);
-}).listen(8080);
+  file.serve(req, res);
+}).listen(3000);
 
-console.log('Server running on port 8080');
+console.log('Server running on port 3000');
