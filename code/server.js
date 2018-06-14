11ed4840121f2816f8a0f36773025ddfb9342e42
buccaneer33 <buccaneer33@inbox.ru>
2018-06-14 16:04:05 +0400

    docker-ok, build-ok, server-nok

--- code/server.js
+++ code/server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 var static = require('node-static');
-var file = new static.Server('./dist', { cache: 0 });
+var file = new static.Server('/dist', { cache: 0 });
 
 http.createServer(function(req, res) {
   file.serve(req, res);
-}).listen(3000);
+}).listen(8080);
 
-console.log('Server running on port 3000');
+console.log('Server running on port 8080');
